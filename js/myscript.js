b2b4d8cf9706e46e2b22b1f068ae6012f7e8bec3
Giuseppe <giuseppe.acito.ga@gmail.com>
2022-05-03 15:37:54 +0200

    password

---
+++ js/myscript.js
@@ -0,0 +1,13 @@
+let miavariabile = prompt("nome");
+console.log (miavariabile);
+
+let miavariabile1 = prompt("cognome");
+console.log (miavariabile1);
+
+let miavariabile2 = prompt("colore preferito");
+console.log (miavariabile2);
+
+const variabilenumero= 21;
+
+let password = `la password è: ${miavariabile + miavariabile1 + miavariabile2 +variabilenumero}`
+console.log (password)
